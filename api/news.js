e37f5af3762b591d223bca202909cc123de7dd8e
lennonbioq <lennonbioq@gmail.com>
2026-02-11 10:19:47 -0300

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,13 +1,13 @@
 export default async function handler(req, res) {
   const sources = [
-    { name: "STF", url: "https://www.stf.jus.br/portal/rss/noticiaRss.asp" },
-    { name: "STJ", url: "https://www.stj.jus.br/sites/portalp/RSS" },
-    { name: "TST", url: "https://www.tst.jus.br/rss" },
-    { name: "CNJ", url: "https://www.cnj.jus.br/feed/" },
-    { name: "Agência Brasil", url: "https://agenciabrasil.ebc.com.br/rss/justica" },
-    { name: "Conjur", url: "https://www.conjur.com.br/rss.xml" },
-    { name: "Migalhas", url: "https://www.migalhas.com.br/rss" }
-  ];
+  { name: "TST", url: "https://www.tst.jus.br/rss" },
+  { name: "CNJ", url: "https://www.cnj.jus.br/feed/" },
+  { name: "Conjur", url: "https://www.conjur.com.br/rss.xml" },
+  { name: "Agência Brasil", url: "https://agenciabrasil.ebc.com.br/rss/justica" },
+  { name: "G1 Política", url: "https://g1.globo.com/rss/g1/politica/" },
+  { name: "Jota", url: "https://www.jota.info/feed" }
+];
+
 
   let allNews = [];
 
